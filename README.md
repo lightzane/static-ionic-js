cb1ba81d35298f4691142785488f4452291eaa3a
lightzane <bhest.pat@gmail.com>
2022-02-10 01:19:03 +0800

    initial

---
+++ README.md
@@ -0,0 +1,24 @@
+# Static Ionic Js
+
+![](https://img.shields.io/badge/@ionic-6-lightskyblue)
+
+Learning and exploring Ionic framework with HTML and vanilla javascript
+
+**Ionic Framework CDN**
+
+```html
+<script type="module" src="https://cdn.jsdelivr.net/npm/@ionic/core/dist/ionic/ionic.esm.js"></script>
+<script nomodule src="https://cdn.jsdelivr.net/npm/@ionic/core/dist/ionic/ionic.js"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@ionic/core/css/ionic.bundle.css" />
+```
+
+## Ionic Documentation
+
+- https://ionicframework.com/docs/
+- https://ionicframework.com/docs/intro/cdn
+- https://ionicframework.com/docs/components
+- https://ionicframework.com/docs/theming/colors
+
+## Reference
+
+- [Ionic 4 & Angular Tutorial For Beginners - Crash Course](https://www.youtube.com/watch?v=r2ga-iXS5i4)
---
+++ app.js
@@ -0,0 +1,54 @@
+const reasonInput = document.querySelector('#input-reason');
+const amountInput = document.querySelector('#input-amount');
+const cancelBtn = document.querySelector('#btn-cancel');
+const confirmBtn = document.querySelector('#btn-confirm');
+const expensesList = document.querySelector('#expenses-list');
+const totalExpensesOutput = document.querySelector('#total-expenses');
+
+let totalExpenses = 0;
+
+const clear = () => {
+    reasonInput.value = null;
+    amountInput.value = null;
+};
+
+confirmBtn.addEventListener('click', () => {
+    const enteredReason = reasonInput.value;
+    const enteredAmount = amountInput.value;
+
+    if (
+        enteredReason?.trim().length <= 0 ||
+        enteredAmount <= 0 ||
+        enteredAmount?.trim().length <= 0
+    ) {
+        presentAlert();
+        return;
+    }
+
+    const newItem = document.createElement('ion-item');
+    newItem.textContent = `${enteredReason}: $${enteredAmount}`;
+
+    expensesList.appendChild(newItem);
+    totalExpenses += +enteredAmount;
+    totalExpensesOutput.textContent = totalExpenses;
+    clear();
+});
+
+cancelBtn.addEventListener('click', () => {
+    clear();
+});
+
+const presentAlert = async () => {
+    const alert = document.createElement('ion-alert');
+    alert.cssClass = 'my-custom-class';
+    alert.header = 'Invalid inputs';
+    alert.subHeader = 'Alert message';
+    alert.message = 'This is an alert message.';
+    alert.buttons = ['Okay', 'Alright'];
+
+    document.body.appendChild(alert);
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+};
